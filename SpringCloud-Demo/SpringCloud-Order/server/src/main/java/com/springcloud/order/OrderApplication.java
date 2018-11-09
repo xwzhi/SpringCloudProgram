93f797496b2b341019d950b40d4760e2aec5d661
624244232 <624244232@qq.com>
2018-11-10 01:35:50 +0800

    Zuul权限过滤及分流

--- SpringCloud-Demo/SpringCloud-Order/server/src/main/java/com/springcloud/order/OrderApplication.java
+++ SpringCloud-Demo/SpringCloud-Order/server/src/main/java/com/springcloud/order/OrderApplication.java
@@ -1,14 +1,23 @@
 package com.springcloud.order;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 //把服务端的接口放在了客户端
 @EnableFeignClients(basePackages = "com.springcloud.product.client")
+@ComponentScan(basePackages = "com.springcloud") //product服务配置了组件，所以进行包扫描o
+@EnableHystrix
 @SpringCloudApplication  //@SpringBootApplication @EnableDiscoveryClient 客户端 @EnableCircuitBreaker Hystrix 启动注解
+@EnableHystrixDashboard  //hystrix可视化界面  http://localhost:8082/hystrix
 //@EnableBinding(StreamInput.class)
 public class OrderApplication {
 
@@ -21,5 +30,16 @@ public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
+
+	//#Hystrix可視化监控 默认有上下文 http://localhost:8082/actuator/hystrix.stream
+	//@Bean
+	/*public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}*/
 
 }
--- SpringCloud-Demo/SpringCloud-Product/client/src/main/java/com/springcloud/product/client/ProductClient.java
+++ SpringCloud-Demo/SpringCloud-Product/client/src/main/java/com/springcloud/product/client/ProductClient.java
@@ -3,17 +3,18 @@ package com.springcloud.product.client;
 import com.spring.product.common.DecreaseStockInput;
 import com.spring.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 /**
- *@Author XuWeiZhi
- *@Description
- *@Date 2018/10/9 23:48
+ * @Author XuWeiZhi
+ * @Description 服务端熔断和应用通信
+ * @Date 2018/10/9 23:48
  **/
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientBack.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -21,4 +22,19 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    /*因为order引入了依赖，所以需要对order应用配置包扫描,方法错误，进行熔断调用下面的方法*/
+    @Component
+    public static class ProductClientBack implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
